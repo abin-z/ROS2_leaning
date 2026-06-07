bc2b527e1cd71a61d2e5d84414e657b7834e547a
abin <abin.z@outlook.com>
2026-06-08 00:00:23 +0800

    perf: 使用lambda优化std::bind

--- src/first_cpp/src/listener.cpp
+++ src/first_cpp/src/listener.cpp
@@ -3,14 +3,19 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 
-using std::placeholders::_1;
+// using std::placeholders::_1;
 
 class Listener : public rclcpp::Node
 {
  public:
   Listener() : Node("cpp_listener")
   {
-    sub_ = this->create_subscription<std_msgs::msg::String>("chatter", 10, std::bind(&Listener::callback, this, _1));
+    // sub_ = this->create_subscription<std_msgs::msg::String>("chatter", 10, std::bind(&Listener::callback, this, _1));
+    // 使用 lambda 替换 std::bind, 但回调函数还是独立的
+    sub_ = this->create_subscription<std_msgs::msg::String>("chatter", 10,
+                                                            [this](const std_msgs::msg::String::SharedPtr msg) {
+                                                              this->callback(msg);  // lambda 里调用成员函数
+                                                            });
   }
 
  private:
